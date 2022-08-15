60ecc417da2894a820c6a38db3a54c0c982ec3ab
francisco-iriarte <iriartef@gmail.com>
2022-08-15 19:13:45 -0300

    Update ejercicio_4.py
    Ejercicio terminado

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -9,6 +9,9 @@
 
 # Ejercicios con bucles "while"
 
+from operator import and_
+
+
 x = 0
 # Realizar un bucle "while" cuya condición de continuidad
 # sea que <x sea menor a 10> y que <x sea distinto de 6>
@@ -16,11 +19,24 @@ x = 0
 # una condición compuesta (utilice el operador "and" o "or" según corresponda)
 # En cada iteracion del bucle debe incrementar el valor de "x" en "2"
 # e imprimir en pantalla el resultado de X (antes de incrementar) con print
+while x < 10 and x != 6:
+    print('Valor x=', x)
+    x +=2   # Incrementar x en 2
+
+print('terminó el bucle while')
+
 
 # Realice el mismo bucle "while" pero en vez de estar formado por una condición
 # compuesta, que el "while" siga iterando mientras <x sea menos a 10>, y dentro del
 # "while" consultar si <x es igual a 6>, y en ese caso realizar una interrupción del bucle
 # En cada iteracion del bucle debe incrementar el valor de "x" en "2"
 # e imprimir en pantalla el resultado de X (antes de incrementar) con print
+x = 0
+while x < 10:
+    if x == 6:
+        print('Bucle interrumpido en x=', x)
+        break
 
+    print('Valor x=', x)
+    x += 2   
 print("terminamos!")
